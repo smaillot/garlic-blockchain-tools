e6b813ed29180c8847c6629f884013c5f86d943b
Stéphane Maillot <s.maillot2@gmail.com>
2018-01-29 20:49:49 +0100

    solving transaction input issue

--- garlic-explorer/blockchain_explore.py
+++ garlic-explorer/blockchain_explore.py
@@ -4,7 +4,9 @@ import numpy as np
 import matplotlib.pylab as plt
 import json
 from os import listdir
+from os import mkdir
 from os.path import isfile, join
+import shutil
 from time import sleep
 
 
@@ -16,24 +18,60 @@ def check_address_tx(addresses, tx):
         output:
             amount exchanged in this transaction
     """
-    transaction = get_rawtransaction(tx)
-    received = {}
     
-    if transaction != {}:
+    transaction = get_rawtransaction(tx)
         
-        time = transaction['blocktime']
-        # check if address is a sender
-            ## doesn't currently work
-            # vin = transaction['vin']
-            # senders = extract_exchanges(vin)
-            # sent = [s[1] for s in senders if s[0] in addresses]
+    def get_received(addresses, transaction):
         
-        # check if the address is a receiver
-        vout = transaction['vout']
-        receivers = extract_exchanges(vout)
-        received = [{'time': time, 'address': r[0], 'value': r[1]} for r in receivers if r[0] in addresses]
+        received = []
         
-    return received
+        if transaction != {}:
+            
+            time = transaction['blocktime']
+            
+            # check one of the addresses is a receiver
+            vout = transaction['vout']
+            receivers = extract_exchanges(vout)
+            received = [{'time': time, 'address': r[0], 'value': r[1]} for r in receivers if r[0] in addresses]
+            
+        return received
+    
+    def get_sent(addresses, transaction):
+        
+        senders = []
+        
+        if transaction != {}:
+            
+            vin = transaction['vin']
+            print('\t\t\tSearching sells in ' + str(len(vin)) + ' inputs')
+            i = 1
+            # check one of the addresses is a receiver
+            for t in vin:
+                if 'txid' in t.items():
+                    print('\t\t\t' + str(i))
+                    txid = t['txid']
+                    transaction = get_rawtransaction(txid)
+                    n_vout = t['vout']
+                    vout = transaction['vout'][n_vout]
+                    senders.append({'time': transaction['blocktime'], 'address': vout['scriptPubKey']['addresses'][0], 'value': -vout['value']})
+                    i += 1
+            
+        return senders
+            
+    received = get_received(addresses, transaction)
+    if received != []:
+        sent = get_sent(addresses, transaction)
+    else:
+        sent = []
+    found = received + sent
+    stacked = []
+    
+    for a in addresses:
+        txs = [f for f in found if f['address'] == a]
+        if txs != []:
+            stacked.append({'address': a, 'time': np.max([t['time'] for t in txs]), 'value': np.sum([t['value'] for t in txs])})
+        
+    return stacked
 
 def data_file_name(n_block):
     return 'data_' + str(n_block) + '.json'
@@ -44,7 +82,10 @@ def extract_block_number(filename):
 def search_transactions(addresses, start=0, end=-1, history='[]', saving=100):
     """ Read all blockchain from begining to list transactions where this address appears
     """
+    
     def save_history(hist, block=''):
+        shutil.rmtree('./data') 
+        os.mkdir('./data')
         with open('./data/' + data_file_name(block), 'w') as outfile:
             json.dump(json.dumps(hist), outfile)
     
@@ -90,24 +131,37 @@ def plot_history(history, series={}):
     
     return ax
 
+def load_history():
+
+    datafiles = [extract_block_number(f) for f in listdir('./data') if isfile(join('./data', f)) and f.split('.')[-1] == 'json' and f.split('_')[0] == 'data']
+    last = 0
+    if datafiles != []:
+        last = np.max(datafiles)
+        last_file = data_file_name(last)
+        
+        with open('./data/' + last_file, 'r') as data_file:
+            history = json.load(data_file)
+                
+    else:
+        history = '[]'
+    
+    return [history, last]
+            
+                       
+
 def live_plot():
     
-    datafiles = [extract_block_number(f) for f in listdir('./data') if isfile(join('./data', f)) and f.split('.')[-1] == 'json' and f.split('_')[0] == 'data']
-    last = np.max(datafiles)
-    last_file = data_file_name(last)
-    
-    with open('./data/' + last_file, 'r') as data_file:
-        history = json.load(data_file)
-        
+    print('Loading history...')
+    [history, last] = load_history() 
+    print('Opening addresses files...')
     addresses = np.load('./addresses/addresses.npy')
+    with open('./addresses/series.json', 'r') as series_file:
+        series = json.load(series_file)
     print('Updating history...\n\n')
     history = search_transactions(addresses, last, history=history)
     last = history['blockheight']
     history = history['history']
     print('History up to date.')
-    
-    with open('./addresses/series.json', 'r') as series_file:
-        series = json.load(series_file)
     
     while 1:
         
